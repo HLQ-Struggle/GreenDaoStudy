502115599706c38f65246d382cf946646a3f17d5
HLQ <925954424@qq.com>
2017-06-06 09:08:23 +0800

    update error

--- app/src/main/java/cn/hlq/greendaostudy/MainActivity.java
+++ app/src/main/java/cn/hlq/greendaostudy/MainActivity.java
@@ -48,46 +48,60 @@ public class MainActivity extends Activity {
         findViewById(R.id.id_insert).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Student stu = new Student(null, "001", "贺da宝", "男孩", "50");
-                long end = stuDao.insert(stu);
-                String msg = "";
-                if (end > 0) {
-                    msg = "001新增成功~";
-                } else {
-                    msg = "新增失败~";
+                try {
+                    Student stu = new Student(null, "001", "贺da宝", "男孩", "50");
+                    long end = stuDao.insert(stu);
+                    String msg = "";
+                    if (end > 0) {
+                        msg = "001新增成功~";
+                    } else {
+                        msg = "新增失败~";
+                    }
+                    stuDao.insert(new Student(null, "002", "贺er宝", "男人", "66"));
+                    stuDao.insert(new Student(null, "003", "贺san宝", "爷儿们", "23"));
+                    stuDao.insert(new Student(null, "004", "贺si宝", "男人", "65"));
+                    Toast.makeText(self, "001 002 003 004新增成功~", Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(self, "学员编号唯一，新增失败~", Toast.LENGTH_SHORT).show();
                 }
-                stuDao.insert(new Student(null, "002", "贺er宝", "男人", "66"));
-                stuDao.insert(new Student(null, "003", "贺san宝", "爷儿们", "23"));
-                stuDao.insert(new Student(null, "004", "贺si宝", "男人", "65"));
-                Toast.makeText(self, "001 002 003 004新增成功~", Toast.LENGTH_SHORT).show();
             }
         });
         // 新增List集合数据
         findViewById(R.id.id_insert_list).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                List<Student> stuList = new ArrayList<Student>();
-                stuList.add(new Student(null, "005", "贺利权", "小爷儿们", "43"));
-                stuList.add(new Student(null, "006", "贺利权", "大爷儿们", "35"));
-                stuList.add(new Student(null, "007", "贺利权", "老爷儿们", "99"));
-                stuList.add(new Student(null, "008", "贺利权", "老少爷儿们", "88"));
-                stuDao.insertInTx(stuList);
-                Toast.makeText(self, "新增成功~", Toast.LENGTH_SHORT).show();
+                try {
+                    List<Student> stuList = new ArrayList<Student>();
+                    stuList.add(new Student(null, "005", "贺利权", "小爷儿们", "43"));
+                    stuList.add(new Student(null, "006", "贺利权", "大爷儿们", "35"));
+                    stuList.add(new Student(null, "007", "贺利权", "老爷儿们", "99"));
+                    stuList.add(new Student(null, "008", "贺利权", "老少爷儿们", "88"));
+                    stuDao.insertInTx(stuList);
+                    Toast.makeText(self, "新增成功~", Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(self, "学员编号唯一，新增失败~", Toast.LENGTH_SHORT).show();
+                }
             }
         });
         // 查询所有
         findViewById(R.id.id_search_all).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                List<Student> stuList = stuDao.queryBuilder().list();
-                if (stuList != null) {
-                    String searchAllInfo = "";
-                    for (int i = 0; i < stuList.size(); i++) {
-                        Student stu = stuList.get(i);
-                        searchAllInfo += "id：" + stu.getStuId() + "编号：" + stu.getStuNo() + "姓名：" + stu.getStuName() + "性别：" + stu.getStuSex() + "成绩：" + stu.getStuScore() + "\n";
+                try {
+                    List<Student> stuList = stuDao.queryBuilder().list();
+                    if (stuList != null) {
+                        String searchAllInfo = "";
+                        for (int i = 0; i < stuList.size(); i++) {
+                            Student stu = stuList.get(i);
+                            searchAllInfo += "id：" + stu.getStuId() + "编号：" + stu.getStuNo() + "姓名：" + stu.getStuName() + "性别：" + stu.getStuSex() + "成绩：" + stu.getStuScore() + "\n";
+                        }
+                        TextView tvSearchInfo = (TextView) findViewById(R.id.id_search_all_info);
+                        tvSearchInfo.setText(searchAllInfo);
                     }
-                    TextView tvSearchInfo = (TextView) findViewById(R.id.id_search_all_info);
-                    tvSearchInfo.setText(searchAllInfo);
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
         });
@@ -95,73 +109,93 @@ public class MainActivity extends Activity {
         findViewById(R.id.id_search_assign).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String searchAssignInfo = "";
-                List<Student> stuList = stuDao.queryBuilder().where(StudentDao.Properties.StuName.eq("贺da宝")).list();
-                for (int i = 0; i < stuList.size(); i++) {
-                    Student stu = stuList.get(i);
-                    searchAssignInfo += "id：" + stu.getStuId() + "编号：" + stu.getStuNo() + "姓名：" + stu.getStuName() + "性别：" + stu.getStuSex() + "成绩：" + stu.getStuScore() + "\n";
+                try {
+                    String searchAssignInfo = "";
+                    List<Student> stuList = stuDao.queryBuilder().where(StudentDao.Properties.StuName.eq("贺da宝")).list();
+                    for (int i = 0; i < stuList.size(); i++) {
+                        Student stu = stuList.get(i);
+                        searchAssignInfo += "id：" + stu.getStuId() + "编号：" + stu.getStuNo() + "姓名：" + stu.getStuName() + "性别：" + stu.getStuSex() + "成绩：" + stu.getStuScore() + "\n";
+                    }
+                    TextView tvSearchAssign = (TextView) findViewById(R.id.id_search_assign_info);
+                    tvSearchAssign.setText(searchAssignInfo);
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-                TextView tvSearchAssign = (TextView) findViewById(R.id.id_search_assign_info);
-                tvSearchAssign.setText(searchAssignInfo);
             }
         });
         // 查询指定数据 查询姓名为"贺da宝"的信息并按照成绩排序-降序
         findViewById(R.id.id_search_assign_order_desc).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String searchAssignOrderDesc = "";
-                List<Student> stuList = stuDao.queryBuilder().where(StudentDao.Properties.StuName.eq("贺利权")).orderDesc(StudentDao.Properties.StuScore).list();
-                for (int i = 0; i < stuList.size(); i++) {
-                    Student stu = stuList.get(i);
-                    searchAssignOrderDesc += "id：" + stu.getStuId() + "编号：" + stu.getStuNo() + "姓名：" + stu.getStuName() + "性别：" + stu.getStuSex() + "成绩：" + stu.getStuScore();
+                try {
+                    String searchAssignOrderDesc = "";
+                    List<Student> stuList = stuDao.queryBuilder().where(StudentDao.Properties.StuName.eq("贺利权")).orderDesc(StudentDao.Properties.StuScore).list();
+                    for (int i = 0; i < stuList.size(); i++) {
+                        Student stu = stuList.get(i);
+                        searchAssignOrderDesc += "id：" + stu.getStuId() + "编号：" + stu.getStuNo() + "姓名：" + stu.getStuName() + "性别：" + stu.getStuSex() + "成绩：" + stu.getStuScore();
+                    }
+                    TextView tvSearchOrderDesc = (TextView) findViewById(R.id.id_search_assign_order_desc_info);
+                    tvSearchOrderDesc.setText(searchAssignOrderDesc);
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-                TextView tvSearchOrderDesc = (TextView) findViewById(R.id.id_search_assign_order_desc_info);
-                tvSearchOrderDesc.setText(searchAssignOrderDesc);
             }
         });
         // 查询指定数据 查询姓名为"贺da宝"的信息并按照成绩排序-升序
         findViewById(R.id.id_search_assign_order_asc).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String searchAssignOrderDesc = "";
-                List<Student> stuList = stuDao.queryBuilder().where(StudentDao.Properties.StuName.eq("贺利权")).orderAsc(StudentDao.Properties.StuScore).list();
-                for (int i = 0; i < stuList.size(); i++) {
-                    Student stu = stuList.get(i);
-                    searchAssignOrderDesc += "id：" + stu.getStuId() + "编号：" + stu.getStuNo() + "姓名：" + stu.getStuName() + "性别：" + stu.getStuSex() + "成绩：" + stu.getStuScore();
+                try {
+                    String searchAssignOrderDesc = "";
+                    List<Student> stuList = stuDao.queryBuilder().where(StudentDao.Properties.StuName.eq("贺利权")).orderAsc(StudentDao.Properties.StuScore).list();
+                    for (int i = 0; i < stuList.size(); i++) {
+                        Student stu = stuList.get(i);
+                        searchAssignOrderDesc += "id：" + stu.getStuId() + "编号：" + stu.getStuNo() + "姓名：" + stu.getStuName() + "性别：" + stu.getStuSex() + "成绩：" + stu.getStuScore();
+                    }
+                    TextView tvSearchOrderDesc = (TextView) findViewById(R.id.id_search_assign_order_asc_info);
+                    tvSearchOrderDesc.setText(searchAssignOrderDesc);
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-                TextView tvSearchOrderDesc = (TextView) findViewById(R.id.id_search_assign_order_asc_info);
-                tvSearchOrderDesc.setText(searchAssignOrderDesc);
             }
         });
         // 组合查询数据 查询姓名为"贺利权" 并且成绩小于等于60
         findViewById(R.id.id_search_combination).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String searchAssignOrderDesc = "";
-                QueryBuilder<Student> stuQB = stuDao.queryBuilder();
-                stuQB.where(StudentDao.Properties.StuName.eq("贺利权"), StudentDao.Properties.StuScore.le("60"));
-                List<Student> stuList = stuQB.list();
-                for (int i = 0; i < stuList.size(); i++) {
-                    Student stu = stuList.get(i);
-                    searchAssignOrderDesc += "id：" + stu.getStuId() + "编号：" + stu.getStuNo() + "姓名：" + stu.getStuName() + "性别：" + stu.getStuSex() + "成绩：" + stu.getStuScore() + "\n";
+                try {
+                    String searchAssignOrderDesc = "";
+                    QueryBuilder<Student> stuQB = stuDao.queryBuilder();
+                    stuQB.where(StudentDao.Properties.StuName.eq("贺利权"), StudentDao.Properties.StuScore.le("60"));
+                    List<Student> stuList = stuQB.list();
+                    for (int i = 0; i < stuList.size(); i++) {
+                        Student stu = stuList.get(i);
+                        searchAssignOrderDesc += "id：" + stu.getStuId() + "编号：" + stu.getStuNo() + "姓名：" + stu.getStuName() + "性别：" + stu.getStuSex() + "成绩：" + stu.getStuScore() + "\n";
+                    }
+                    TextView tvSearchOrderDesc = (TextView) findViewById(R.id.id_search_combination_info);
+                    tvSearchOrderDesc.setText(searchAssignOrderDesc);
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-                TextView tvSearchOrderDesc = (TextView) findViewById(R.id.id_search_combination_info);
-                tvSearchOrderDesc.setText(searchAssignOrderDesc);
             }
         });
         // 查询所有返回数据 但只返回前三条数据
         findViewById(R.id.id_search_limit).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                List<Student> stuList = stuDao.queryBuilder().limit(3).list();
-                if (stuList != null) {
-                    String searchAllInfo = "";
-                    for (int i = 0; i < stuList.size(); i++) {
-                        Student stu = stuList.get(i);
-                        searchAllInfo += "id：" + stu.getStuId() + "编号：" + stu.getStuNo() + "姓名：" + stu.getStuName() + "性别：" + stu.getStuSex() + "成绩：" + stu.getStuScore() + "\n";
+                try {
+                    List<Student> stuList = stuDao.queryBuilder().limit(3).list();
+                    if (stuList != null) {
+                        String searchAllInfo = "";
+                        for (int i = 0; i < stuList.size(); i++) {
+                            Student stu = stuList.get(i);
+                            searchAllInfo += "id：" + stu.getStuId() + "编号：" + stu.getStuNo() + "姓名：" + stu.getStuName() + "性别：" + stu.getStuSex() + "成绩：" + stu.getStuScore() + "\n";
+                        }
+                        TextView tvSearchInfo = (TextView) findViewById(R.id.id_search_limit_info);
+                        tvSearchInfo.setText(searchAllInfo);
                     }
-                    TextView tvSearchInfo = (TextView) findViewById(R.id.id_search_limit_info);
-                    tvSearchInfo.setText(searchAllInfo);
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
         });
@@ -169,15 +203,19 @@ public class MainActivity extends Activity {
         findViewById(R.id.id_search_limit_offset).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                List<Student> stuList = stuDao.queryBuilder().limit(3).offset(1).list();
-                if (stuList != null) {
-                    String searchAllInfo = "";
-                    for (int i = 0; i < stuList.size(); i++) {
-                        Student stu = stuList.get(i);
-                        searchAllInfo += "id：" + stu.getStuId() + "编号：" + stu.getStuNo() + "姓名：" + stu.getStuName() + "性别：" + stu.getStuSex() + "成绩：" + stu.getStuScore() + "\n";
+                try {
+                    List<Student> stuList = stuDao.queryBuilder().limit(3).offset(1).list();
+                    if (stuList != null) {
+                        String searchAllInfo = "";
+                        for (int i = 0; i < stuList.size(); i++) {
+                            Student stu = stuList.get(i);
+                            searchAllInfo += "id：" + stu.getStuId() + "编号：" + stu.getStuNo() + "姓名：" + stu.getStuName() + "性别：" + stu.getStuSex() + "成绩：" + stu.getStuScore() + "\n";
+                        }
+                        TextView tvSearchInfo = (TextView) findViewById(R.id.id_search_limit_offset_info);
+                        tvSearchInfo.setText(searchAllInfo);
                     }
-                    TextView tvSearchInfo = (TextView) findViewById(R.id.id_search_limit_offset_info);
-                    tvSearchInfo.setText(searchAllInfo);
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
         });
@@ -185,31 +223,43 @@ public class MainActivity extends Activity {
         findViewById(R.id.id_search_count).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int stuSumCount = stuDao.queryBuilder().list().size();
-                TextView tvSearchInfo = (TextView) findViewById(R.id.id_search_count_info);
-                tvSearchInfo.setText(stuSumCount);
+                try {
+                    int stuSumCount = stuDao.queryBuilder().list().size();
+                    TextView tvSearchInfo = (TextView) findViewById(R.id.id_search_count_info);
+                    tvSearchInfo.setText(stuSumCount + "");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         });
         // 删除指定信息
         findViewById(R.id.id_delete).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                stuDao.queryBuilder().where(StudentDao.Properties.StuName.eq("贺si宝")).buildDelete().executeDeleteWithoutDetachingEntities();
-                Toast.makeText(self, "删除成功~", Toast.LENGTH_SHORT).show();
+                try {
+                    stuDao.queryBuilder().where(StudentDao.Properties.StuName.eq("贺si宝")).buildDelete().executeDeleteWithoutDetachingEntities();
+                    Toast.makeText(self, "删除成功~", Toast.LENGTH_SHORT).show();
 //                stuDao.delete(new Student()); // 删除指定对象
 //                stuDao.deleteAll(); // 删除所有
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         });
         // 更新指定信息
         findViewById(R.id.id_update).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Student student = stuDao.queryBuilder().where(StudentDao.Properties.StuName.eq("贺da宝")).build().unique();
-                if (student != null) {
-                    student.setStuName("I Love You");
-                    stuDao.update(student);
+                try {
+                    Student student = stuDao.queryBuilder().where(StudentDao.Properties.StuName.eq("贺da宝")).build().unique();
+                    if (student != null) {
+                        student.setStuName("I Love You");
+                        stuDao.update(student);
+                    }
+                    Toast.makeText(self, "更新成功~", Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-                Toast.makeText(self, "更新成功~", Toast.LENGTH_SHORT).show();
             }
         });
     }
